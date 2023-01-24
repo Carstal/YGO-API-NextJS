36d13658701201346db3e868a08952eef60afa9b
Carstal <43147122+Carstal@users.noreply.github.com>
2023-01-24 14:38:46 -0500

    get props

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,18 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+export async function getServerSideProps() {
+  const res = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?&startdate=03/08/2002&enddate=03/09/2002&dateregion=tcg_date");
+  const cards = await res.json();
+  console.log(cards)
+  return { props: { cards }};
+}
+
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>LOB API </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
