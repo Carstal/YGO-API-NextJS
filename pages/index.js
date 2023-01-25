976a33521001860b3a8dd5b0cdc84dcf72c5ade3
Carstal <43147122+Carstal@users.noreply.github.com>
2023-01-24 21:08:51 -0500

    fixed issues?

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,23 @@
 import Head from 'next/head'
-import styles from '../../styles/ygo.module.css'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 export async function getServerSideProps() {
-    const res = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?&startdate=03/08/2002&enddate=03/09/2002&dateregion=tcg_date");
-    const cards = await res.json();
-    // console.log(cards.data);
-    return { props: { cards }};
+  const res = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?&startdate=03/08/2002&enddate=03/09/2002&dateregion=tcg_date");
+  const cards = await res.json();
+
+  return { props: { cards }};
 }
 
-// function getCard(card){
-    // const type = card.type
-    // console.log(type)
-
-//     <div className='selectedST'>
-//     <div className='imgDiv'>
-//     </div>
-//     <div className='cardInfo'>
-//       <p>
-//         Name: Black Pendant
-//         {/* {selectedName}<br/> */}
-//         <br/>
-//         Type: Equip Spell
-//         {/* {selectedType}<br/> */}
-//         <br/>
-//         Desc: Equipped monster gains 500 Attack
-//         {/* {selectedDesc}<br/> */}
-//       </p>
-//     </div>
-//   </div>
-//     return (
-//     <div className='selectedM'>
-//     <div className={styles.img}>
-//                     <img src={card.card_images[0].image_url_large}
-//                     alt="Card Image"/>
-//     </div>
-//     <div className='cardInfo'>
-//     <p>
-//         Name: {card.name}
-//         <br/>
-//         Type: {card.type}
-//         <br/>
-//         Level: {card.level}
-//         <br/>
-//         Type: {card.race}
-//         <br/>
-//         Attribute: {card.attribute}
-//         <br/>
-//         Desc: {card.desc}
-//         <br/>
-//         Attack: {card.atk}
-//         <br/>
-//         Defense: {card.def}
-//     </p>
-//     </div>
-// </div>);
-// }
-
 export default function Home({cards}) {
-    return (
+  return (
     <div className="container">
-        <Head>
+      <Head>
         <title>LOB API </title>
         <link rel="icon" href="/favicon.ico" />
-        </Head>
+      </Head>
 
-        <main>
+      <main>
         <header>LOB Cards</header>
         <div className={styles.container}>
         {cards.data.map((card) => (
@@ -81,7 +34,9 @@ export default function Home({cards}) {
             </div>
         ))}
         </div>
-        </main>
+      </main>
+
+      {/* <Footer /> */}
     </div>
-    )
+  )
 }
